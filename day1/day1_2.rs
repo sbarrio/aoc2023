98e3154adfa324df3c5967b5f4e36705cf5a514c
Sergio Barrio <sergio.barrio.slocker@gmail.com>
2023-12-01 17:52:00 +0100

    Cleanup

--- day1/day1_2.rs
+++ day1/day1_2.rs
@@ -6,7 +6,7 @@ use std::process;
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: Vec<String> = env::args().collect();
     if args.len() < 2 {
-        eprintln!("Usage: ./day <filename>");
+        eprintln!("Usage: ./day1_2 <filename>");
         process::exit(1);
     }
 
